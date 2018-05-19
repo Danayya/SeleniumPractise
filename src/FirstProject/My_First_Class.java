51b5220e1f0d34ada98ca814b6074c1d7824d54e
Danayya <39435492+Danayya@users.noreply.github.com>
2018-05-19 18:47:01 +0530

    My First personal GIT sync

---
+++ src/FirstProject/My_First_Class.java
@@ -0,0 +1,14 @@
+package FirstProject;
+
+public class My_First_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=101;i++)
+		{
+			System.out.println(i+".Shree Guru Fakkireshwara Prasanna");
+		}
+
+	}
+
+}
